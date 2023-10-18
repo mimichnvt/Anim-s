d48dc59c2be38b1d784db09de0317d1a922536b9
Emilie <emilie.chnvt@gmail.com>
2023-10-18 12:45:44 +0200

    add animation

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ var menu_toggle = document.querySelector('.menu_toggle');
 
 menu_toggle.onclick = function(){
     menu_toggle.classList.toggle('active');
+    menu.classList.toggle('responsive')
 }
+
+//site animtion
+
+const header = document.querySelector('header');
+const title_span = document.querySelectorAll('.left h1 span');
+const p = document.querySelector('.left p');
+const a = document.querySelector('.left a');
+const img = document.querySelector('.right .image-bird')
+
+window.addEventListener('load',()=>{
+
+    const TL = gsap.timeline({paused: true});
+    TL
+    .staggerFrom(header , 2 , {y:-100 , opacity:0, ease: "power2.out"}, 0.1 )
+    .staggerFrom(img , 1 , {x:-1000 , opacity:0, ease: "power2.out"}, 0.3 )
+    .staggerFrom(title_span , 1 , {opacity:0, ease: "power2.out"}, 0.1 )
+    .staggerFrom(p, 1 , {opacity:0, ease: "power2.out"}, 0.2 )
+    .staggerFrom(a, 1 , {opacity:0, ease: "power2.out"}, 0.3 )
+    
+
+    TL.play()
+})
+
+
+
+
+
 
 
 
